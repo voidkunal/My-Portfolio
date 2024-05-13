0a0ab19d4c251ef274e4ffaa4ce914b35c844d77
void hack <kunalmandal1607@gmail.com>
2024-05-13 11:40:42 +0530

    make js part lite

--- script.js
+++ script.js
@@ -12,24 +12,6 @@ icon.addEventListener("click", () => {
     }
 });
 
-/******************************************** Download button ********************************************/
-
-// const downloadLinks = document.querySelectorAll("[data-download]");
-// downloadLinks.forEach(button => {
-//     const id = button.CDATA_SECTION_NODE.download;
-//     const image = document.getElementById(id);
-//     const a = document.createElement("a");
-
-//     a.herf = image.scroll;
-//     a.download= " ";
-//     a.style.display = "none";
-
-//     button.addEventListener("click", () =>{
-//         document.body.appendChild(a);
-//         a.click();
-//         document.body.removeChild(a);
-//     });
-// });
 
 /******************************************** toggle icon navbar ********************************************/
 
